ebb4016fc5f5b265f5afd13256f5d87d469690da
clavay <clavayssiere@univ-pau.fr>
2021-11-26 15:23:38 +0100

    fix device with various subdevices

--- pyscada/laborenergie/worker.py
+++ pyscada/laborenergie/worker.py
@@ -4,6 +4,7 @@
 from __future__ import unicode_literals
 
 from pyscada.utils.scheduler import SingleDeviceDAQProcessWorker
+from pyscada.laborenergie import PROTOCOL_ID
 
 import logging
 
@@ -11,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 class Process(SingleDeviceDAQProcessWorker):
-    device_filter = dict(modbusdevice__pysensedevice__isnull=False)
+    device_filter = dict(modbusdevice__pysensedevice__isnull=False, protocol_id=PROTOCOL_ID)
     bp_label = 'pyscada.laborenergie-%s'
 
     def __init__(self, dt=5, **kwargs):
